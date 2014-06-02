f17979378663094665a07e59b2bc9ac2e6de9f70
Daniel Dinnyes <ddinnyes@fedora-desktop.home.net>
2014-06-02 10:55:29 +0100

    added working function and macro expansion, anonymous function creation, and if statement

--- src/myjvm/lisp.clj
+++ src/myjvm/lisp.clj
@@ -29,17 +29,23 @@
     (assoc ctx name (conj bind-stack form))))
 
 (defn push-all-bindings
-  [ctx binding-form]
-  (if-let [bindings (list-form-seq binding-form)]
-    (if-not (odd? (count bindings))
-      (reduce (fn [m [sym expr]]
-                (if-not (= :symbol (:type sym))
-                  (throw (ex-info "Cannot bind to anything but symbols") {})
-                  (push-binding m (:name sym) (lisp-eval ctx expr))))
-              ctx
-              (partition 2 bindings))
-      (throw (ex-info "Bindings should be symbol/expression pairs" {})))
-    (throw (ex-info "Binding should be a list" {}))))
+  [ctx bindings]
+  (if-not (odd? (count bindings))
+    (reduce (fn [m [sym expr]]
+              (if-not (= :symbol (:type sym))
+                (throw (ex-info "Cannot bind to anything but symbols") {})
+                (push-binding m (:name sym) (lisp-eval ctx expr))))
+            ctx
+            (partition 2 bindings))
+    (throw (ex-info "Bindings should be symbol/expression pairs" {}))))
+
+(defn lisp-eval-fn
+  [{:keys [ctx params body]} args]
+  (if (= (count args)
+         (count params))
+    (let [bindings (interleave params args)
+          local-ctx (push-all-bindings ctx bindings)]
+      (lisp-eval local-ctx body))))
 
 (defn lisp-eval
   [ctx form]
@@ -48,17 +54,45 @@
     (resolve-symbol ctx (:name form))
     :list
     (let [head-form (:head form)]
-      (if (= :symbol (:type head-form))
+      (if (and (= :symbol (:type head-form))
+               (contains? #{"quote" "let" "if" "fn"} (:name head-form)))
         (case (:name head-form)
           "quote" (:tail form)
           "let"
           (let [tail-forms (list-form-seq (:tail form))]
             (if (= 2 (count tail-forms))
-              (let [new-ctx (push-all-bindings ctx (first tail-forms))]
-                (lisp-eval new-ctx (second tail-forms)))
+              (if-let [bindings (list-form-seq (first tail-forms))]
+                (let [local-ctx (push-all-bindings ctx bindings)]
+                  (lisp-eval local-ctx (second tail-forms)))
+                (throw (ex-info "Bindings should be a list" {})))
               (throw (ex-info "Let statement has to have bindings and an expression." {}))))
-          "if" (throw (ex-info "Unimplemented" {}))
-          "fn" (throw (ex-info "Unimplemented" {})))))
+          "if"
+          (let [tail-forms (list-form-seq (:tail form))]
+            (if (= 3 (count tail-forms))
+              (if (not= :nil (:type (lisp-eval ctx (nth tail-forms 0))))
+                (lisp-eval ctx (nth tail-forms 1))
+                (lisp-eval ctx (nth tail-forms 2)))
+              (throw (ex-info "If statement has to have 3 parts"))))
+          "fn"
+          (let [tail-forms (list-form-seq (:tail form))]
+            (if (and (= 2 (count tail-forms))
+                     (every? (fn [f] (= :symbol (:type f)))
+                             (first tail-forms)))
+              {:type :func
+               :ctx ctx
+               :params (first tail-forms)
+               :body (second tail-forms)})))
+        (let [evaled-hf (lisp-eval ctx head-form)]
+          (case (:type evaled-hf)
+            :func
+            (let [arg-forms (list-form-seq (:tail form))]
+              (lisp-eval-fn
+               evaled-hf
+               (vec (map #(lisp-eval ctx %) arg-forms))))
+            :macro
+            (let [arg-forms (list-form-seq (:tail form))
+                  macro-expand-form (lisp-eval-fn evaled-hf arg-forms)]
+              (lisp-eval ctx macro-expand-form))))))
     form))
 
 (deftest test-lisp-eval
@@ -72,9 +106,21 @@
                       :head {:type :symbol :name "test"}
                       :tail {:type :list
                              :head {:type :int :value 1}
-                             :tail {:type :nil}}}
+                             :tail {:type :list
+                                    :head {:type :symbol :name "false"}
+                                    :tail {:type :list
+                                           :head {:type :nil}
+                                           :tail {:type :nil}}}}}
                :tail {:type :list
-                      :head {:type :symbol :name "test"}
+                      :head {:type :list
+                             :head {:type :symbol :name "if"}
+                             :tail {:type :list
+                                    :head {:type :symbol :name "false"}
+                                    :tail {:type :list
+                                           :head {:type :symbol :name "test"}
+                                           :tail {:type :list
+                                                  :head {:type :int :value -1}
+                                                  :tail {:type :nil}}}}}
                       :tail {:type :nil}}}})))
 
 
